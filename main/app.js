661bb23e79f59a3241e6cd7a5f3a1abaaa25db91
sam tiso <samtiso@Sammys-computer.local>
2022-01-26 16:11:28 -0500

    scroll working with jquery, some responsive design added

--- main/app.js
+++ main/app.js
@@ -3,14 +3,25 @@ console.log("Hello");
 $(() => {
     console.log('javaScript loaded ');
 
-    let aboutButton = $('aboutButton');
+    // let aboutButton = $('#about-button');
 
-    aboutButton.on('click', scrollToAbout);
+    // aboutButton.on('click', scrollToAbout);
 
-    function scrollToAbout() {
-        $('.about-me').scrollTop(80);
-    }
+    // function scrollToAbout() {
+    //     $('body').animate(
+    //         // {scrollTop: $('#about-area').offset.top()}, 3000
+    //         scrollTo($('#about-area'), 3000)
+    //         // scrollTop($('#about-area'), 3000)
+    //     );
+                
+    // }
+
 
     
+        $('#about-button').click(function() {
+        $('html, body').animate({
+          scrollTop: $("#about-area").offset().top
+        }, 1000) 
+        })
 
 });
